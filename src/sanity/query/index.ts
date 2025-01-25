6bd2811364480d2f84a6651adef5e3a80551f6db
Milan <milanthegamer@gmail.com>
2025-01-25 13:36:17 +0400

    feat: category added

--- src/sanity/query/index.ts
+++ src/sanity/query/index.ts
@@ -1,7 +1,42 @@
+import { isNumber } from "@/lib/utils";
+
 export const GET_TOP_STORY_LIST = `*[_type == "article"] | order(publishedDate desc)[0...6]{
+    "slug": slug.current, 
     title, 
     description, 
-    slug, 
+    publishedDate, 
+    "heroImage": heroImage.asset,
+    "author": author -> name, 
+    "category": category -> name
+}`;
+
+export const GET_ARTICLE = (
+  slug: string
+) => `*[_type == "article" && slug.current == "${slug}" ]{
+    "slug": slug.current, 
+    title, 
+    description, 
+    publishedDate, 
+    "heroImage": heroImage.asset,
+    "author": author -> name, 
+    "category": category -> name,
+    body
+}`;
+
+export const GET_ALL_CATEGORIES = `*[_type == "category"]{
+    "slug": slug.current, 
+    name, 
+    description,
+}`;
+
+export const GET_ARTICLES_BY_CATEGORY = ({
+  slug,
+  from,
+  to,
+}: any) => `*[_type == "article" && category->slug.current == "${slug}"] ${isNumber(from) && isNumber(to) ? `[${from}...${to}]` : ""} {
+    "slug": slug.current, 
+    title, 
+    description, 
     publishedDate, 
     "heroImage": heroImage.asset,
     "author": author -> name, 
